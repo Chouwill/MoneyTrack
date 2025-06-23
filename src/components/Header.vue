d310cdf6c7f42ded21e1ae85015c9de5c4d7d085
willchou93068 <will_chou@rsl.com.tw>
2025-06-23 13:12:02 +0800

    feat(header): 完成 Header 切版（LOGO + 登入/註冊按鈕），僅含畫面樣式，不含表單功能

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,41 +1,20 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <header class="flex w-full  p-4">
+    <div class="logo w-[50%] flex justify-center items-center">
+      <img src="https://picsum.photos/id/70/70/70" alt="" />
+    </div>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    <div class="member-center w-[50%]  flex justify-around items-center md:justify-end md:px-4">
+      <div class="member-link flex gap-5">
+        <router-link to="/login" class="bg-[#5b86e5] text-[white] rounded-lg border px-5 py-2">登入</router-link>
+        <router-link to="/register" class="bg-[white] text-[#5b86e5] rounded-lg border px-5 py-2">註冊</router-link>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
