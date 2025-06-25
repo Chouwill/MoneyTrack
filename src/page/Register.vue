ebcbd38688028988c75f9c665c6e2925cc2d76d5
Chouwill <will92068@gmail.com>
2025-06-25 15:13:11 +0800

    feat(register): 註冊頁面加入 Zod 的單欄驗證與整體驗證功能

--- src/page/Register.vue
+++ src/page/Register.vue
@@ -1,9 +1,69 @@
 <script setup lang="ts">
-import InputText from "primevue/inputtext";
+import { z } from "zod";
 
 import { ref } from "vue";
+import InputText from "primevue/inputtext";
+import { email } from "zod/v4";
+import { Password } from "primevue";
 
-const value = ref(null);
+const formData = ref({
+  email: "",
+  password: "",
+  confirmPassword: "",
+});
+
+const errorMessage = ref({
+  email: "",
+  password: "",
+  confirmPassword: "",
+  text: "",
+});
+
+const registerSchema = z.object({
+  email: z.string().email({ message: "email必填" }).nonempty(),
+  password: z
+    .string({ required_error: "密碼必填" })
+    .min(1, { message: "密碼必填" })
+    .max(8, { message: "密碼最多 8 位數" }),
+  confirmPassword: z.string().max(8).nonempty({ message: "請再次輸入密碼" }),
+});
+
+const checkField = (field: any) => {
+  console.log("單欄驗證", formData.value);
+  const fieldResult = registerSchema.shape[field].safeParse(formData.value[field]);
+
+  console.log(fieldResult);
+
+  if (fieldResult.success === true) {
+    console.log("驗證成功");
+  } else {
+    console.log("驗證失敗");
+
+    const errMsg = fieldResult.error.format();
+
+    console.log(errMsg);
+
+    errorMessage.value[field] = errMsg._errors[0];
+
+    console.log(errorMessage.value.email);
+  }
+};
+
+const handleUserRegister = () => {
+  console.log(formData.value);
+  const result = registerSchema.safeParse(formData.value);
+
+  console.log(result);
+  if (result.success === true) {
+    console.log("驗證true");
+  } else {
+    console.log("驗證false");
+
+    errorMessage.value.text = '註冊失敗';
+
+    console.log(errorMessage.value.email);
+  }
+};
 </script>
 
 <template>
@@ -11,18 +71,27 @@ const value = ref(null);
     <form class="w-full border flex flex-col justify-center items-center gap-10 py-10">
       <div class="flex flex-col gap-1">
         <label class="block" for="email">帳號</label>
-        <InputText class="w-full rounded-xl" type="text" />
+        <InputText class="w-full rounded-xl" type="text" v-model="formData.email" @blur="checkField('email')" />
+        <p class="errorText" v-if="errorMessage.email">{{ errorMessage.email }}</p>
       </div>
       <div class="flex flex-col gap-1">
         <label class="block" for="password">密碼</label>
-        <InputText class="w-full rounded-xl" type="text " />
+        <InputText class="w-full rounded-xl" type="text " v-model="formData.password" @blur="checkField('password')" />
+        <p class="errorText" v-if="errorMessage.password">{{ errorMessage.password }}</p>
       </div>
       <div class="flex flex-col gap-1">
         <label class="block" for="password">確認密碼</label>
-        <InputText class="w-full rounded-xl" type="text " />
+        <InputText
+          class="w-full rounded-xl"
+          type="text "
+          v-model="formData.confirmPassword"
+          @blur="checkField('confirmPassword')" />
+        <p class="errorText" v-if="errorMessage.confirmPassword">{{ errorMessage.confirmPassword }}</p>
       </div>
+      <p class="errorText" v-if="errorMessage.text">{{ errorMessage.text }}</p>
+
       <div class="w-full flex justify-center border py-2">
-        <router-link to="/login" class="bg-[#5b86e5] text-[white] rounded-lg border px-5 py-2">註冊</router-link>
+        <button @click="handleUserRegister" class="bg-[#5b86e5] text-[white] rounded-lg border px-5 py-2">註冊</button>
       </div>
     </form>
   </div>
