eeddaf3dce15483f6e5500f38cece1df59a79d8c
Chouwill <will92068@gmail.com>
2025-07-05 18:52:49 +0800

    feat(index): 串接圖卡 GET API 並顯示資料

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header class="flex w-full  p-4">
+  <header class="flex w-full p-4">
     <div class="logo w-[50%] flex justify-center items-center">
-      <img src="https://picsum.photos/id/70/70/70" alt="" />
+      <router-link to="/index">
+        <img src="https://picsum.photos/id/70/70/70" alt="" />
+      </router-link>
     </div>
 
-    <div class="member-center w-[50%]  flex justify-around items-center md:justify-end md:px-4">
+    <div
+      class="member-center w-[50%] flex justify-around items-center md:justify-end md:px-4"
+    >
       <div class="member-link flex gap-5">
-        <router-link to="/login" class="bg-[#5b86e5] text-[white] rounded-lg border px-5 py-2">登入</router-link>
-        <router-link to="/register" class="bg-[white] text-[#5b86e5] rounded-lg border px-5 py-2">註冊</router-link>
+        <router-link
+          to="/login"
+          class="bg-[#5b86e5] text-[white] rounded-lg border px-5 py-2"
+          >登入</router-link
+        >
+        <router-link
+          to="/register"
+          class="bg-[white] text-[#5b86e5] rounded-lg border px-5 py-2"
+          >註冊</router-link
+        >
       </div>
     </div>
   </header>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
