cf01c260275a1a3463872cba4f0b1d5588499ac9
Chouwill <will92068@gmail.com>
2025-07-08 14:10:17 +0800

    feat: 安裝並註冊 Pinia 狀態管理

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { createPinia } from 'pinia'
 
+
+const pinia = createPinia()
 import PrimeVue from "primevue/config";
 import Aura from "@primeuix/themes/aura";
 import router from "./router";
@@ -9,6 +12,8 @@ import router from "./router";
 
 const app = createApp(App);
 app.use(router);
+app.use(pinia)
+ㄔㄛ
 
 app.use(PrimeVue, {
   theme: {
