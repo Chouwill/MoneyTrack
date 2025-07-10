2090f8c2c1af2b8aeaf9e314f577738317fdadd1
Chouwill <will92068@gmail.com>
2025-07-10 21:50:34 +0800

    feat(admin): 調整圖卡樣式並加入分頁器元件

--- src/page/AdminPanel.vue
+++ src/page/AdminPanel.vue
@@ -4,6 +4,9 @@ import { ref, onMounted } from "vue";
 
 // import { CustomerService } from "@/service/CustomerService";
 import { FilterMatchMode, FilterOperator } from "@primevue/core/api";
+import Button from "primevue/button";
+import Paginator from "primevue/paginator";
+
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import ColumnGroup from "primevue/columngroup"; // optional
@@ -35,17 +38,33 @@ const products = ref();
 </script>
 
 <template>
-  <div class="card">
-    <DataTable :value="datas" tableStyle="min-width: 50rem">
-      <Column field="code" header="日期"></Column>
-      <Column field="name" header="名稱"></Column>
-      <Column field="action" header="動作"></Column>
-      <Column field="quantity" header="數量"></Column>
-      <Column field="unitprice" header="單價"></Column>
-      <Column field="totalcost" header="總額"></Column>
-      <Column field="ispublic" header="是否公開"></Column>
+  <div class="container">
+    <div class="button"></div>
+    <div
+      class="card-box border flex flex-wrap gap-2 justify-around items-center my-5"
+    >
+      <div
+        v-for="item in datas"
+        :key="item.id"
+        class="IndexCard w-xs my-2 flex  flex-col justify-center items-center md:p-5"
+      >
+        <h2 class="card-title">名稱：{{ item.name }}</h2>
+        <h2 class="card-title">代號：{{ item.code }}</h2>
+        <h2 class="card-title">交易動作：{{ item.action }}</h2>
+        <h2 class="card-title">日期：{{ item.date }}</h2>
+        <h2 class="card-title">數量：{{ item.quantity }}</h2>
+        <h2 class="card-title">總花費：{{ item.totalcost }}</h2>
+        <h2 class="card-title">備註：第一次投資高股息！</h2>
 
-      <Column field="attachment" header="附件"></Column>
-    </DataTable>
+        <Button class="my-4" label="顯示" />
+      </div>
+      <div class="page w-[100%]">
+        <Paginator
+          :rows="10"
+          :totalRecords="120"
+          :rowsPerPageOptions="[10, 20, 30]"
+        ></Paginator>
+      </div>
+    </div>
   </div>
 </template>
