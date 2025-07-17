9b86f15364df513888c5632debbd7c583625607f
Chouwill <will92068@gmail.com>
2025-07-17 18:37:49 +0800

    refactor(register): 重構表單變數命名與 Zod 驗證邏輯

--- src/components/DialogRegister.vue
+++ src/components/DialogRegister.vue
@@ -7,28 +7,31 @@ import InputText from "primevue/inputtext";
 
 // const visible = ref(false);
 
-import { onLogin } from "../api/method";
+import { onRegister } from "../api/method";
 
 import { z } from "zod";
 import cookie from "../util/useCookie";
 import type { display } from "@primeuix/themes/aura/inplace";
 
-const loginSchema = z.object({
+const registerSchema = z.object({
   email: z.string().email({ message: "email必填" }).nonempty(),
   password: z
     .string({ required_error: "密碼必填" })
     .min(1, { message: "密碼必填" })
     .max(8, { message: "密碼最多 8 位數" }),
+  confirmPassword: z.string().max(8).nonempty({ message: "請再次輸入密碼" }),
 });
 
 const formData = ref({
   email: "",
   password: "",
+  confirmPassword: "",
 });
 
 const errorMessage = ref({
   email: "",
   password: "",
+  confirmPassword: "",
   text: "",
 });
 
@@ -36,52 +39,48 @@ type fieldType = "password" | "email";
 
 const checkField = (field: fieldType) => {
   console.log("單欄驗證", formData.value);
-  console.log("field", field);
+  const fieldResult = registerSchema.shape[field].safeParse(
+    formData.value[field]
+  );
 
-  const result = loginSchema.shape[field].safeParse(formData.value[field]);
+  console.log(fieldResult);
 
-  console.log("單欄驗證", result);
-
-  if (result.success === true) {
-    console.log("true");
+  if (fieldResult.success === true) {
+    console.log("驗證成功");
     errorMessage.value[field] = "";
   } else {
-    console.log("false");
+    console.log("驗證失敗");
 
-    const errMsg = result.error.format();
+    const errMsg = fieldResult.error.format();
 
     console.log(errMsg);
 
     errorMessage.value[field] = errMsg._errors[0];
 
-    console.log(errorMessage.value.password);
+    console.log(errorMessage.value.email);
   }
 };
 
-const handleLogin = async () => {
+const handleUserRegister = async () => {
   console.log(formData.value);
+  const result = registerSchema.safeParse(formData.value);
 
-  const loginResult = loginSchema.safeParse(formData.value);
-
-  console.log(loginResult);
-
-  if (loginResult.success === true) {
-    console.log("驗證成功");
+  console.log(result);
+  if (result.success === true) {
+    console.log("驗證true");
     try {
-      const res = await onLogin(formData.value);
+      const res = await onRegister(formData.value);
 
-      console.log(res);
-      console.log("token", res.data.token);
-
-      const token = res.data.token;
-
-      cookie.set(token);
-    } catch (error) {
-      console.log(error);
+      console.log("註冊api回傳", res);
+    } catch (e) {
+      console.log(e);
     }
   } else {
     console.log("驗證false");
-    errorMessage.value.text = "登入失敗";
+
+    errorMessage.value.text = "註冊失敗";
+
+    console.log(errorMessage.value.email);
   }
 };
 
@@ -143,18 +142,18 @@ defineExpose({ visible });
               class="w-full rounded-xl"
               placeholder="確認密碼"
               type="text"
-              v-model="formData.password"
-              @blur="checkField('password')"
+              v-model="formData.confirmPassword"
+              @blur="checkField('confirmPassword')"
             />
 
-            <p class="errorText" v-if="errorMessage.password">
-              {{ errorMessage.password }}
+            <p class="errorText" v-if="errorMessage.confirmPassword">
+              {{ errorMessage.confirmPassword }}
             </p>
           </div>
           <div class="w-[80%] flex justify-center border py-2">
             <button
               class="bg-[#DEE33E] w-full text-[black] text-base rounded-lg border px-5 py-2"
-              @click="handleLogin"
+              @click="handleUserRegister"
             >
               註冊
             </button>
